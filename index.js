3d9780987fae5723ac6325108c767c1da93fec4e
eacazares <eacazar@gmail.com>
2023-04-24 10:31:07 -0700

    adding home page view

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
-app.get ('/', function(req,res){
-    res.status(404).send('hello world')
+app.get('/', (req, res) => {
+    res.render('home')
 })
+
+// app.get ('/', function(req,res){
+//     res.status(404).send('hello world')
+// })
 
 app.get('*', (req, res) => {
     res.send('<h1>404 Page</h1>')
