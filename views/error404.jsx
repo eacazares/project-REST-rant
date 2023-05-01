4a154728c055fc13d4335e8f459d61091153dee4
eacazares <eacazar@gmail.com>
2023-04-30 19:39:07 -0700

    adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src = "/images/flowers.jpg" alt = "flowers"/>
+                    <div> Photo by <a href="https://unsplash.com/es/@ryunosuke_kikuno?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ryunosuke Kikuno</a> on <a href="https://unsplash.com/images/nature/flower?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+  
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,14 @@ function home() {
         <Def>
             <main>
                 <h1>Home</h1>
+                <div>
+                    <img src = "/images/pasta.jpg" alt = "Spaghetti"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/ja/@mggbox?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mgg Vitchakorn</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href = '/places'>
-                    <button className = 'btn-primary'>Places Page</button>
+                    <button className = 'btn btn-primary'>Places Page</button>
                 </a>
             </main>
         </Def>
