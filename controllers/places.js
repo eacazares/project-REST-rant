a3ce483b02b24b9efe4e8eb04840f3ad8dcb4780
eacazares <eacazar@gmail.com>
2023-05-04 20:41:07 -0700

    delete funcitonality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
-
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -18,9 +10,19 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', {place: places[id]})
+    res.render('places/show', {place: places[id], id})
   }
 })
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+
 
 router.post('/', (req, res) => {
   if (!req.body.pic) {
@@ -36,6 +38,21 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,38 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name}</h1>
+            <img src = {data.place.pic}/>
+            
+            <div>
+            <h3>Description</h3>
+            <h4>{data.place.city}</h4>
+            <h4>{data.place.state}</h4>
+            <h4>{data.place.cuisines}</h4>
+            </div>
+
+            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <button type="submit" className='btn btn-danger'>
+                    Delete
+                </button>
+            </form>
+
+            <div>
+                <h3>Rating</h3>
+                <p>Not Rated</p>
+            </div>
+
+            <div>
+                <h3>Comments</h3>
+                <p>No comments yet</p>
+            </div>
           </main>
         </Def>
     )
